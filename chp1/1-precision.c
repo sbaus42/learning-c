b1a8bdcce4079c1133559d962b6e54297b953619
Santiago Baus <sbaus87@gmail.com>
2017-11-21 11:18:42 -0500

    playing with fahrenheit

---
+++ chp1/1-precision.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+  float fi, sec;
+  fi = 20;
+  sec = -123456.2;
+
+  printf("%4.0f,%6.2f\n", fi, sec);
+}
---
+++ chp1/fah-to-cel.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+  float fahr, celsius;
+  int lower, upper, step;
+
+  lower = 0;
+  upper = 300;
+  step = 20;
+
+  printf("  F to C\n");
+  printf("----------\n");
+
+  fahr = lower;
+  while (fahr <= upper) {
+    celsius = (5.0/9.0) * (fahr-32.0);
+    printf("%3.0f %6.1f\n", fahr, celsius);
+    fahr = fahr + step;
+  }
+
+  printf("\n\n  C to F\n");
+  printf("----------\n");
+
+  float cel;
+  cel = lower;
+  while (cel <= upper) {
+    fahr = (9.0 / 5.0) * cel + 32.0;
+    printf("%3.0f %6.1f\n", cel, fahr);
+    cel = cel + step;
+  }  
+}
