bda1c1238c1b93e580a5bb3799b6757b155fde73
Santiago Baus <sbaus87@gmail.com>
2017-11-21 11:29:01 -0500

    removes swp file and adds gitignore

--- chp1/fah-to-cel.c
+++ chp1/fah-to-cel.c
@@ -12,11 +12,11 @@ int main()
   printf("  F to C\n");
   printf("----------\n");
 
-  fahr = lower;
-  while (fahr <= upper) {
+  fahr = upper;
+  while (fahr >= lower) {
     celsius = (5.0/9.0) * (fahr-32.0);
     printf("%3.0f %6.1f\n", fahr, celsius);
-    fahr = fahr + step;
+    fahr = fahr - step;
   }
 
   printf("\n\n  C to F\n");
